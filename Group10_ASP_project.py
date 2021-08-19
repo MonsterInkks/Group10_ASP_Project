e1459824708ecfb766b25411708200aeefe63f59
Jeremy Teng <Joel.9898(@hotmail.com>
2021-08-19 10:56:43 +0800

    project

--- Group10_ASP_project.py
+++ Group10_ASP_project.py
@@ -4,7 +4,4 @@ import matplotlib.pyplot as plt
 
 Country = pd.read_excel('IMVA.xls')
 
-Countries = Country['Periods'].str.split(' ',n = 1,expand = True)
-Country = Country.assign(Year=Countries[1])
-Country.index = Country['Year']
 print(Country)
